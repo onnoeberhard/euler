30b621a018d66b8c0dc6a715253a8c4f9ea1ce85
Onno Eberhard <onnoeberhard@gmail.com>
2022-08-20 12:00:17 +0200

    Change Problem 35

--- p035.hs
+++ p035.hs
@@ -1,10 +1,10 @@
-import Data.Numbers.Primes (isPrime)
+import Data.Numbers.Primes (isPrime, primes)
 
 isCirc :: Int -> Bool
 isCirc = all (isPrime . read) . circs . show
 
 circs s = s : takeWhile (/= s) [tail x ++ [head x] | x <- circs s]
 
-solution = length . filter isCirc $ [1..1000000]
+solution = length . filter isCirc . takeWhile (< 1000000) $ primes
 
 main = print solution
